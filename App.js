b678a2d7eac442a95b9ce886af311de152a70435
Rashmi Vidushani <rashmividushani2023@gmail.com>
2025-03-20 18:42:38 -0400

    Navgation Stack

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import BookListScreen from './src/screens/BookListScreen';
+import BookDetailScreen from './src/screens/BookDetailScreen';
+import BorrowedBooksScreen from './src/screens/BorrowedBooksScreen';
+import { TouchableOpacity , Text} from 'react-native';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="BookList" component={BookListScreen} options={{ 
+          title: 'Available Books',
+          headerStyle: { backgroundColor: '#121212' },
+          headerTitleStyle: { color: '#FFC107' },
+          headerTitleAlign: 'center',
+           }} />
+        <Stack.Screen name="BookDetail" component={BookDetailScreen} options={{ 
+          title: 'Book Details',
+          headerStyle: { backgroundColor: '#121212' },
+          headerTintColor: '#FFC107',
+          headerTitleStyle: { color: '#FFC107' },
+          headerTitleAlign: 'center',
+          }} />
+        <Stack.Screen name="BorrowedBooks" component={BorrowedBooksScreen} options={{ 
+          title: 'Borrowed Books',
+          headerStyle: { backgroundColor: '#121212' },
+          headerTitleStyle: { color: '#FFC107' },
+          headerTintColor: '#FFC107',
+          headerTitleAlign: 'center', }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
